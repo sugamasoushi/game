0cde5a58277fc5661998111e64852e78eb913461
sugamasoushi <sugamasoushi@gmail.com>
2026-07-13 22:04:15 +0900

    冒頭注記の判定

--- app/(game)/scenes/SceneController.ts
+++ app/(game)/scenes/SceneController.ts
@@ -187,7 +187,7 @@ export class SceneController extends Scene {
         this.scene.stop('Event');
         this.scene.stop('Battle');
         this.scene.stop('UI');
-        
+
         this.scene.stop('SceneController');
         this.scene.start('Boot');
 
@@ -206,7 +206,7 @@ export class SceneController extends Scene {
         const gameHeight = Number(this.game.config.height)
 
         //ブラウザ版スマホの場合は注記を表示
-        if (executionEnvironment.isBowserSmartPhone()) {
+        if (!executionEnvironment.isPWA() || !executionEnvironment.isElectron() || !executionEnvironment.isBowserPC()) {
             const gameStartAlert = this.add.image(gameWidth / 2, gameHeight / 2, 'GameStartAlert');
 
             const tapStart = this.add.text(
